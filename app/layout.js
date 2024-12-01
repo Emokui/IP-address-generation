3fa065a08c999287776f7718795d252c1b598a28
Sukurain <136175327+Emokui@users.noreply.github.com>
2024-12-01 21:26:25 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="zh">
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌍</text></svg>" />
+           <link rel="icon" href="./icon.png">
       </head>
       <body>
         <Theme appearance="light" accentColor="blue" grayColor="slate" radius="medium" scaling="100%">
